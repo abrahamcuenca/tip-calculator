474f91083fe0be47accb374f61e3c418c3fd6253
Abraham Cuenca <websiteprojectmanager@gmail.com>
2023-09-09 21:29:47 -0700

    Update readme and minor refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let bill;
 let people;
-let percent = 0;
+let percent;
 let total;
 
 const billAmount= document.getElementById('billAmount');
@@ -10,57 +10,78 @@ const resetButton = document.getElementById('resetButton');
 const tipButtons = document.querySelector('.tipButtons');
 const tipTotal = document.getElementById('tipAmount');
 const personTotal= document.getElementById('totalAmount');
+const peopleErrorMessage = document.getElementById('peopleErrorMessage');
+const billErrorMessage = document.getElementById('billErrorMessage');
 
-const calcTip = () => {
-    const billDivided = bill / people ;
-    const tip = (billDivided * percent) / 100;
-    const totalPerPerson = billDivided + tip;
-
-    tipTotal.textContent = new Intl.
-    NumberFormat('en-US', {
+const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD'
-    })
-        .format(tip);
-
-    personTotal.textContent = new Intl.
-    NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    })
-        .format(totalPerPerson)
+    }).format(amount);
 }
 
+const calcTip = () => {
+    if (bill > 0 && people > 0 && percent > 0) {
+
+        const billDivided = bill / people;
+        const tip = (billDivided * percent) / 100;
+        const totalPerPerson = billDivided + tip;
+
+        tipTotal.textContent = formatCurrency(tip);
+        personTotal.textContent = formatCurrency(totalPerPerson);
+    }
+}
 
 const reset = () => {
-    tipTotal.textContent = "";
-    personTotal.textContent = "";
+    tipTotal.textContent = "$0.00";
+    personTotal.textContent = "$0.00";
+
+    billErrorMessage.classList.add('visually-hidden');
+    peopleErrorMessage.classList.add('visually-hidden');
+    billAmount.classList.remove('error')
+    numberOfPeople.classList.remove('error')
+
     billAmount.value = "";
-    numberOfPeople.value = "";
-    customTipAmount.value = ""
-    bill = 0;
-    people = 0;
-    percent = 0;
-    total = 0;
+    numberOfPeople.value = null;
+    customTipAmount.value = null;
+    bill = null;
+    people = null;
+    percent = null;
+    total = null;
+}
+
+const validator = (element) => {
+    if(element.value === null || element.value === '' || Number(element.value) > 0) {
+        element.parentElement.parentElement.childNodes[3].classList.add('visually-hidden');
+        element.classList.remove('error');
+    }
+    else {
+        element.classList.add('error');
+        element.parentElement.parentElement.childNodes[3].classList.remove('visually-hidden');
+    }
 }
 
 billAmount.onkeyup = (e) => {
-    bill = e.target.value;
+    validator(e.target);
+    bill = e.target.value || null;
 }
 
-numberOfPeople.onblur = (e) => {
-    people = e.target.value;
-    personTotal.textContent = bill / people;
+numberOfPeople.onkeyup = (e) => {
+    validator(e.target);
+    people = e.target.value || null;
+
+    if (bill > 0 && people > 0) {
+        personTotal.textContent = formatCurrency((bill / people));
+    }
 }
 
-customTipAmount.onblur = (e) => {
+customTipAmount.onkeyup = (e) => {
     percent = e.target.value
-    calcTip();
 }
 
 tipButtons.onclick = (e) => {
     percent = e.target.value;
-    if (percent && bill && people) {
+    if (percent && bill && people ) {
         calcTip();
     }
 }
